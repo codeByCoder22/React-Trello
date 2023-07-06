0a633c47ac14b47b0ddfbc526d5446c74b7aac68
WuQiang <wuqiang5733@gmail.com>
2023-07-06 14:48:58 +0930

    singleton SocketService

--- src/shared/services/socket.service.ts
+++ src/shared/services/socket.service.ts
@@ -1,41 +1,63 @@
-import { useEffect, useRef } from "react";
-import { CurrentUserInterface } from "../../auth/types/currentUser.interface";
 import { io, Socket } from "socket.io-client";
+import { CurrentUserInterface } from "../../auth/types/currentUser.interface";
 
-export function useSocket(currentUser: CurrentUserInterface) {
-    const socketRef = useRef<Socket | null>(null);
+class SocketService {
+    private socket: Socket | undefined;
+    private static instance: SocketService;
 
-    useEffect(() => {
-        socketRef.current = io("http://localhost:4001", {
+    // private constructor() {
+    //     this.initializeSocket();
+    // }
+
+    // private initializeSocket(): void {
+    //     this.socket = io("http://localhost:4001");
+    // }
+
+    public static getInstance(): SocketService {
+        if (!SocketService.instance) {
+            SocketService.instance = new SocketService();
+        }
+        return SocketService.instance;
+    }
+
+    setupSocketConnection(currentUser: CurrentUserInterface): void {
+        // if (this.socket) {
+        //     this.socket.disconnect();
+        // }
+
+        this.socket = io("http://localhost:4001", {
             auth: {
                 token: currentUser.token,
             },
         });
-
-        return () => {
-            if (socketRef.current) {
-                socketRef.current.disconnect();
-            }
-        };
-    }, [currentUser]);
-
-    function emit(eventName: string, message: any): void {
-        if (!socketRef.current) {
-            throw new Error("Socket connection is not established");
-        }
-        socketRef.current.emit(eventName, message);
     }
 
-    function listen<T>(eventName: string, callback: (data: T) => void): void {
-        const socket = socketRef.current;
+    disconnect(): void {
+        if (!this.socket) {
+            throw new Error("Socket connection is not established");
+        }
+        this.socket.disconnect();
+    }
+
+    emit(eventName: string, message: any): void {
+        if (!this.socket) {
+            throw new Error("Socket connection is not established");
+        }
+        this.socket.emit(eventName, message);
+    }
+
+    listen(eventName: string, callback: (data: any) => void): void {
+        const socket = this.socket;
         if (!socket) {
             throw new Error("Socket connection is not established");
         }
 
-        socket.on(eventName, (data: T) => {
+        socket.on(eventName, (data) => {
             callback(data);
         });
     }
-
-    return { emit, listen };
 }
+
+const socketService = SocketService.getInstance();
+
+export default socketService;
