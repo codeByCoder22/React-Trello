f15be499c2c98cc7460d10aa402ec21e8da4cc47
WuQiang <wuqiang5733@gmail.com>
2023-07-06 23:49:40 +0930

    InlineFormComponent

--- src/shared/services/boards.service.ts
+++ src/shared/services/boards.service.ts
@@ -2,5 +2,11 @@ import { BoardInterface } from "../types/board.interface";
 import axiosInstance from "../../utils/axiosInstance";
 
 export const getBoards = async (): Promise<BoardInterface[]> => {
-    return axiosInstance.get("/boards").then((res) => res.data);
+    return axiosInstance
+        .get("/boards")
+        .then((res) => res.data)
+        .catch((error) => {
+            // Handle the error here
+            throw new Error("Error retrieving boards: " + error.message);
+        });
 };
