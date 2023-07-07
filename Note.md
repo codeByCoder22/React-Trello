5696f905db98c5fb4654e575076619a0a08a91d9
WuQiang <wuqiang5733@gmail.com>
2023-07-07 13:06:58 +0930

    useRef type mismatch

--- Note.md
+++ Note.md
@@ -1,2 +1,33 @@
 [react router dom auth](https://www.robinwieruch.de/react-router-authentication/)
 [react router dom auth](D:\Documents\Web\React\react_router_dom_auth)
+
+Error :
+ERROR in src/shared/components/InlineFormComponent.tsx:74:29
+TS2322: Type 'RefObject<HTMLInputElement | HTMLTextAreaElement>' is not assignable to type 'LegacyRef<HTMLInputElement> | undefined'.  
+ Type 'RefObject<HTMLInputElement | HTMLTextAreaElement>' is not assignable to type 'RefObject<HTMLInputElement>'.
+Type 'HTMLInputElement | HTMLTextAreaElement' is not assignable to type 'HTMLInputElement'.
+Type 'HTMLTextAreaElement' is missing the following properties from type 'HTMLInputElement': accept, align, alt, capture, and 27 more.
+72 | placeholder={inputPlaceholder}
+73 | onKeyDown={handleKeyDown}
+
+> 74 | ref={inputRef}
+
+       |                             ^^^
+    75 |                         />
+    76 |                     )}
+    77 |                     {inputType === "textarea" && (
+
+ERROR in src/shared/components/InlineFormComponent.tsx:85:29
+TS2322: Type 'RefObject<HTMLInputElement | HTMLTextAreaElement>' is not assignable to type 'LegacyRef<HTMLTextAreaElement> | undefined'.  
+ Type 'RefObject<HTMLInputElement | HTMLTextAreaElement>' is not assignable to type 'RefObject<HTMLTextAreaElement>'.
+Type 'HTMLInputElement | HTMLTextAreaElement' is not assignable to type 'HTMLTextAreaElement'.
+Type 'HTMLInputElement' is missing the following properties from type 'HTMLTextAreaElement': cols, rows, textLength, wrap
+83 | className="input-form-input"
+84 | onKeyDown={handleKeyDown}
+
+> 85 | ref={inputRef}
+
+       |                             ^^^
+    86 |                         ></textarea>
+    87 |                     )}
+    88 |                     {hasButton && (
--- src/shared/components/InlineFormComponent.tsx
+++ src/shared/components/InlineFormComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 interface InlineFormProps {
     title?: string;
@@ -21,6 +21,13 @@ const InlineFormComponent: React.FC<InlineFormProps> = ({
 }) => {
     const [isEditing, setIsEditing] = useState(false);
     const [formValue, setFormValue] = useState("");
+    const inputRef = useRef<HTMLInputElement | HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        if (isEditing && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [isEditing]);
 
     const activeEditing = () => {
         setFormValue(title);
@@ -34,6 +41,12 @@ const InlineFormComponent: React.FC<InlineFormProps> = ({
         }
         setIsEditing(false);
         setFormValue("");
+    };
+
+    const handleKeyDown = (event: React.KeyboardEvent) => {
+        if (event.key === "Enter") {
+            onSubmit(event);
+        }
     };
 
     return (
@@ -57,6 +70,8 @@ const InlineFormComponent: React.FC<InlineFormProps> = ({
                             }
                             className="input-form-input"
                             placeholder={inputPlaceholder}
+                            onKeyDown={handleKeyDown}
+                            ref={inputRef}
                         />
                     )}
                     {inputType === "textarea" && (
@@ -66,6 +81,8 @@ const InlineFormComponent: React.FC<InlineFormProps> = ({
                                 setFormValue(event.target.value)
                             }
                             className="input-form-input"
+                            onKeyDown={handleKeyDown}
+                            ref={inputRef}
                         ></textarea>
                     )}
                     {hasButton && (
